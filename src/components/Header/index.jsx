fa6479bf9e6962b19175deb45c96023c55a6c4c0
Mateus Schulle Leite <mateusschulleleite@gmail.com>
2023-05-29 23:48:08 -0300

    cabecalho concluido

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import styles from './Header.module.scss';
+import logo from './logo.png';
+import search from './search.png';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="Logo do Alura Space" />
+      <div className={styles.header__container}>
+        <input className={styles.header__input} type="text" placeholder="O que você procura?" />
+        <img src={search} alt="Ícone de lupa" />
+      </div>
+    </header>
+  );
+}
